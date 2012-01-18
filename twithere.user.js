101c527be923c121f47e6b696340f0ff22f8a79f
binw <binw@10252193131.radm1.ra.johnshopkins.edu>
2012-01-18 18:46:35 +0800

    url to link.

--- twithere.user.js
+++ twithere.user.js
@@ -19,7 +19,10 @@ function twithere(url){
                             '<img src="' + tweet.profile_image_url + '" style="border-radius:5px;" />' +
                         '</div>' +
                         '<div style="margin-left:58px;">' +
-                            tweet.from_user + ': ' + tweet.text +
+                            '<span style="color:black;">'+ 
+                            tweet.from_user + ': '+
+                            '</span>'+ 
+                            linkify(tweet.text) +
                         '</div>' +
                     '</div>'
                 );
@@ -30,12 +33,26 @@ function twithere(url){
         }
     });
 }
-
+function linkify(txt){
+    if(txt)
+    txt = txt.replace(
+	    /((https?\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi,
+        function(_url){
+            var full_url = _url;
+            if (!full_url.match('^https?:\/\/')) {
+               full_url = 'http://' + full_url;
+            }
+            var short_url = _url.replace(/^https?:\/\//,'');
+            return '<a href="'+full_url+'">'+short_url+'</a>';
+        }
+    );
+    return txt;
+}
 if(window.parent == window){ // process only main page, not iframes
     $('body').append(
         '<div id="twithere" style="position:absolute; top:10px; left:10px; width:auto; max-width:400px; color:navy; opacity:0.9; z-index:5566; border-radius:5px; padding:3px; background-image: -moz-linear-gradient(left, rgb(245,142,32) 10%, rgb(255,0,0) 58%); " >' +
         '<a id="twithere_toggle">twithere</a>' + ' ' +
-        '<span id="twithere_count">0</span>' +
+        '[<span id="twithere_count">0</span>]' +
         '<div id="twithere_content" style="display:none; " />' +
         '</div>'
     )
